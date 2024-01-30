3e80bbab04f0b912907de39eaf8df84e43e37570
Jeanine Wiedeback <jmwiedeback@outlook.com>
2024-01-29 20:07:56 -0700

    first commit

---
+++ The-vinyl-countdown/script.js
@@ -0,0 +1,16 @@
+function logOut() {
+  //   clickAlert() = document.querySelector("#login");
+  alert("Log Out");
+}
+
+function removeElement(elem) {
+  elem.remove();
+}
+// alert("Log Out");
+
+// var clickAlert = document.querySelector("#login");
+// console.log(clickAlert);
+
+// function removeElement(elem) {
+//   elem.remove();
+// }
